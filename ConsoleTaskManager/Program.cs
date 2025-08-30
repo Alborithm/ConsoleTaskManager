f1e59bfd47a9916036a487853aaae739a9b6f708
Alborithm <47321228+Alborithm@users.noreply.github.com>
2025-08-29 19:11:19 -0600

    Create new task

--- ConsoleTaskManager/Program.cs
+++ ConsoleTaskManager/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConsoleTaskManager.Models;
+﻿using System.Globalization;
+using ConsoleTaskManager.Models;
 
 var myTask = new Todo()
 {
@@ -12,6 +13,7 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine(myTask.Title);
 
 ConsoleKeyInfo Key = new ConsoleKeyInfo();
+
 // The app flow
 while (Key.KeyChar != 'q')
 {
@@ -20,7 +22,36 @@ while (Key.KeyChar != 'q')
   Console.WriteLine("(2) Create a ToDo");
   Console.WriteLine("(3) Update ToDo");
   Console.WriteLine("(q) OR Ctrl + C to quit");
+
   Key = Console.ReadKey();
-  Console.WriteLine(Key.KeyChar);
+
+  if (Key.KeyChar == '2')
+  {
+    var newTodo = new Todo();
+    string? input;
+    Console.WriteLine("Type the Title:");
+    input = Console.ReadLine()!;
+    newTodo.Title = input.Trim() != "" ? input : "Default title";
+
+    Console.WriteLine("Type de description:");
+    input = Console.ReadLine()!;
+    newTodo.Description = input.Trim() != "" ? input : "No Decription";
+
+    Console.WriteLine("Type the due date in yyyy/mm/dd");
+    input = Console.ReadLine();
+    if (DateTime.TryParseExact(input, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+    {
+      newTodo.DueDate = result;
+    }
+    else
+    {
+      newTodo.DueDate = DateTime.Today;
+    }
+
+    Console.WriteLine(newTodo.Title);
+    Console.WriteLine(newTodo.Description);
+    Console.WriteLine(newTodo.DueDate);
+    Console.WriteLine(newTodo.Status);
+  }
 
 }
