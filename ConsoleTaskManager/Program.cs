174d738dd7fbfa0a05a0ec4ec98092ed17411d2b
Alborithm <47321228+Alborithm@users.noreply.github.com>
2025-08-30 14:48:57 -0600

    Refactor create new task cycle

--- ConsoleTaskManager/Program.cs
+++ ConsoleTaskManager/Program.cs
@@ -28,30 +28,41 @@ while (Key.KeyChar != 'q')
   if (Key.KeyChar == '2')
   {
     var newTodo = new Todo();
-    string? input;
-    Console.WriteLine("Type the Title:");
-    input = Console.ReadLine()!;
-    newTodo.Title = input.Trim() != "" ? input : "Default title";
-
-    Console.WriteLine("Type de description:");
-    input = Console.ReadLine()!;
-    newTodo.Description = input.Trim() != "" ? input : "No Decription";
-
-    Console.WriteLine("Type the due date in yyyy/mm/dd");
-    input = Console.ReadLine();
-    if (DateTime.TryParseExact(input, "yyyy/MM/dd", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+    newTodo.Title = PromptUser("Task title:");
+    newTodo.Description = PromptUser("Task description:");
+    if (DateTime.TryParseExact(
+      PromptUser("Task due date (yyyy/mm/dd):"),
+      "yyyy/MM/dd",
+      CultureInfo.InvariantCulture,
+      DateTimeStyles.None,
+      out DateTime result))
     {
       newTodo.DueDate = result;
     }
-    else
-    {
-      newTodo.DueDate = DateTime.Today;
-    }
-
-    Console.WriteLine(newTodo.Title);
-    Console.WriteLine(newTodo.Description);
-    Console.WriteLine(newTodo.DueDate);
-    Console.WriteLine(newTodo.IsComplete);
   }
 
+}
+
+string PromptUser(string text)
+{
+  Console.WriteLine(text);
+  return Console.ReadLine();
+}
+
+bool PromptTaskCreation()
+{
+  string Title;
+  string Description;
+  string DueDate;
+
+  Console.WriteLine("Type the title:");
+  Title = Console.ReadLine();
+
+  Console.WriteLine("Type the description:");
+  Description = Console.ReadLine();
+
+  Console.WriteLine("Type the due date (yyyy/mm/dd):");
+  DueDate = Console.ReadLine();
+
+  return true;
 }
